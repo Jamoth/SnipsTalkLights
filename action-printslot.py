ffb0e5e38f1a33ac56b395c6593aea71afaec19e
Jamoth <45373054+Jamoth@users.noreply.github.com>
2018-11-26 23:48:14 +0100

    Update action-printslot.py

--- action-printslot.py
+++ action-printslot.py
@@ -2,16 +2,9 @@
 from hermes_python.hermes import Hermes
 ​
 def intent_received(hermes, intent_message):
-    print('Intent {}'.format(intent_message.intent))
-​
-    for (slot_value, slot) in intent_message.slots.items():
-        print('Slot {} -> \n\tRaw: {} \tValue: {}'.format(slot_value, slot[0].raw_value, slot[0].slot_value.value.value))
-        
-        result_sentence= "Da geht was!"
-        current_session_id = intentMessage.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)
-​
-    hermes.publish_end_session(intent_message.session_id, None)
+    result_sentence= "Da geht was!"
+    current_session_id = intentMessage.session_id
+    hermes.publish_end_session(current_session_id, result_sentence)
 ​
 with Hermes('raspberrypi.local:1883') as h:
     h.subscribe_intents(intent_received).start()
