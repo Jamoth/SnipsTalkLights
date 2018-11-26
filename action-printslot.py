722cf28f75a0bafdb1c21d378d7d3434e48292ac
Jamoth <45373054+Jamoth@users.noreply.github.com>
2018-11-26 23:48:26 +0100

    Update action-printslot.py

--- action-printslot.py
+++ action-printslot.py
@@ -2,7 +2,7 @@
 from hermes_python.hermes import Hermes
 ​
 def intent_received(hermes, intent_message):
-    result_sentence= "Da geht was!"
+    result_sentence= "Da geht was lokal!"
     current_session_id = intentMessage.session_id
     hermes.publish_end_session(current_session_id, result_sentence)
 ​
