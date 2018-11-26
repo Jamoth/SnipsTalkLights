e00f4d7b708e9d5373c16a52f16c5026a590affd
Jamoth <45373054+Jamoth@users.noreply.github.com>
2018-11-26 23:23:54 +0100

    Update action-printslot.py

--- action-printslot.py
+++ action-printslot.py
@@ -7,7 +7,7 @@ def intent_received(hermes, intent_message):
     for (slot_value, slot) in intent_message.slots.items():
         print('Slot {} -> \n\tRaw: {} \tValue: {}'.format(slot_value, slot[0].raw_value, slot[0].slot_value.value.value))
         
-        result_sentence= "Licht ist aus!"
+        result_sentence= "Da geht was!"
         current_session_id = intentMessage.session_id
         hermes.publish_end_session(current_session_id, result_sentence)
 ​
