e0b295cdb883d9de674cfa513d89734c0be7985e
Jamoth <45373054+Jamoth@users.noreply.github.com>
2018-11-27 18:39:19 +0100

    Update action-LichtAus.py

--- action-LichtAus.py
+++ action-LichtAus.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 
 import ConfigParser
 from hermes_python.hermes import Hermes
@@ -38,8 +39,8 @@ def action_wrapper(hermes, intentMessage, conf):
     Refer to the documentation for further details.
     """
     if len(intentMessage.slots.objectLocation) > 0:
-        objectLocation = intentMessage.slots.objectLocation.first().value # We extract the value from the slot "house_room"
-        result_sentence = "Schalte das Licht {} aus".format(str(objectLocation))  # The response that will be said out loud by the TTS engine.
+        objectLocation = str8(intentMessage.slots.objectLocation.first().value).encode('utf8')) # We extract the value from the slot "house_room"
+        result_sentence = "Schalte das Licht {} aus".format(objectLocation)  # The response that will be said out loud by the TTS engine.
     else:
         result_sentence = 	"Schalte das Licht aus"
         
