1245228a2612ac953036baa7f6202cd73faaea47
Jamoth <45373054+Jamoth@users.noreply.github.com>
2018-12-01 02:10:00 +0100

    Update action-LichtAn.py

--- action-LichtAn.py
+++ action-LichtAn.py
@@ -51,5 +51,5 @@ def action_wrapper(hermes, intentMessage, conf):
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("Jamoth:LampenAusSchalten", subscribe_intent_callback) \
+        h.subscribe_intent("Jamoth:LampenAnSchalten", subscribe_intent_callback) \
 .start()
