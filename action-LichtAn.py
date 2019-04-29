ffa30d36a7bb9603ef9c3bcf50b1358a2b414977
Jamoth <45373054+Jamoth@users.noreply.github.com>
2019-04-29 11:09:13 +0200

    Update action-LichtAn.py

--- action-LichtAn.py
+++ action-LichtAn.py
@@ -1,34 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import configparser
 from hermes_python.hermes import Hermes
-from hermes_python.ontology import *
-import io
-
-CONFIGURATION_ENCODING_FORMAT = "utf-8"
-CONFIG_INI = "config.ini"
-
-class SnipsConfigParser(configparser.SafeConfigParser):
-    def to_dict(self):
-        return {section: {option_name: option for option_name, option in self.items(section)} for section in
-                self.sections()}
-
-
-
-def read_configuration_file(configuration_file):
-    try:
-        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
-            conf_parser = SnipsConfigParser()
-            conf_parser.readfp(f)
-            return conf_parser.to_dict()
-    except (IOError, ConfigParser.Error) as e:
-        return dict()
-
-def subscribe_intent_callback(hermes, intentMessage):
-    conf = read_configuration_file(CONFIG_INI)
-    action_wrapper(hermes, intentMessage, conf)
-
 
 def action_wrapper(hermes, intentMessage, conf):
     """ Write the body of the function that will be executed once the intent is recognized.
