884ec244198bba3c99896c4aba710cb2b82440ac
Jamoth <45373054+Jamoth@users.noreply.github.com>
2019-04-29 11:34:17 +0200

    Update action-LichtAn.py

--- action-LichtAn.py
+++ action-LichtAn.py
@@ -14,7 +14,7 @@ def action_wrapper(hermes, intentMessage):
     
     if len(intentMessage.slots.objectLocation) > 0:
         objectLocation = ((intentMessage.slots.objectLocation.first().value))
-        result_sentence = u"Schalte das Licht {} an".format(objectLocation.decode('utf-8'))        
+        result_sentence = u"Schalte das Licht {} an".format(objectLocation)        
     else:
         result_sentence = 	"Schalte das Licht an"
         
